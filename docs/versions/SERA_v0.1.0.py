81da3944e260b5c924b291b92dc314e7f02b505c
janvipatelwork529-create <janvipatelwork529@gmail.com>
2026-09-15 22:59:36 +0530

    Compelete SERA v.0.2.0

--- docs/versions/SERA_v0.1.0.py
+++ docs/versions/SERA_v0.1.0.py
@@ -1,56 +1,21 @@
-def greet_user(name):
-    print("=========================================\n=========================================")
-    print("          Project SERA v0.2")
-    print("=========================================\n=======================================")
-    print(f"Hello,{name}")
-    print("I am SERA.\n Your personal Ai assistant")
+print("====================================\n=============")
+print("PROJECT SERA v.01")
+print("====================================\n=============")
+print("Hello! \n I am SERA. \n what is your name?")
+a=input("")
+print("welcome",a)
+print("How can I help you today? \n 1. Study \n 2. Motivation \n 3. About SERA \n 4. Exit")
+c=int(input(""))
 
-def study():
-    print("Let's start learning.")
-
-def motivation():
-    print('KEEP LEARNING UNTIL "L" BECOMES SILENT')
-
-def about_her():
-    print("I am SERA.\n Your new personal Ai Assistant.\n i am still growing")    
-
-def exit():
-    print(f"goodbye,{name}\n See you soon. \n I am shutting down.")
-
-# ======================================================================================       
-
-name=input("enter your name please:")
-greet_user(name)
-running=True
-while running:
-    print("How can I help you today?")
-    print("1.Study \n 2.Motivation \n 3.Information about me \n 4.Exit")
-    choice=int(input("enter your choice:"))
-
-
-    if choice==1:
-        study()
-    elif choice==2:
-        motivation()
-    elif choice==3:
-        about_her()
-    elif choice==4:
-        exit()
-        running=False
-    else:
-        print("invalid choice!!\n please select between 1 to 4")
-
-    if running:
-         print("=========================================\n=========================================")
-         print("Anything else")
-         print("=========================================\n=======================================")
-          
-
-
-
-
+if(c==1):
+   print("Let's study together.\nOne small step every day creates big success.")
+elif(c==2):
+    print("YOUR FUTUR RANK DEPENDS ON TODAYS EFFORTS!")
+elif(c==3):
+    print("I am SERA. your personal AI assistant.I am still learning, and together we will grow")
+elif(c==4):
+    print("Goog Bye",a)
+    print(" i am shuting down.\n see you tomorrpw!")
+else:
+    print("Invalide choice")
     
-
-
-
-
