2cee89fadd9084e7e4bbbbf7ec73f7a29b055742
janvipatelwork529-create <janvipatelwork529@gmail.com>
2026-09-16 21:58:35 +0530

    Complete SERA v.0.3.0

---
+++ docs/versions/v.0.3.0/SERA_v0.3.0.py
@@ -0,0 +1,78 @@
+def greet_user(name):
+    print("=========================================\n=========================================")
+    print("          Project SERA v0.3")
+    print("=========================================\n=======================================")
+    print(f"Hello,{name}")
+    print("I am SERA.\n Your personal Ai assistant")
+
+def study():
+    print("Let's start learning.")
+
+def motivation():
+    print('KEEP LEARNING UNTIL "L" BECOMES SILENT')
+
+def about_her():
+    print("I am SERA.\n Your new personal Ai Assistant.\n i am still growing")    
+
+def exit():
+    print(f"goodbye,{name}\n See you soon. \n I am shutting down.")
+
+# ======================================================================================       
+import os 
+
+if os.path.exists("memory.txt"):
+    with open ("memory.txt","r") as file:
+        name=file.read()
+        print("Welcome back,", name)
+
+else:
+    name=input("What is your name?")
+    with open ("memory.txt","w")as file:
+        file.write(name)
+        print("nice to meet you.", name)       
+
+
+greet_user(name)
+running=True
+while running:
+    print("How can I help you today?")
+    print("1.Study \n 2.Motivation \n 3.Information about me \n 4.Exit")
+    choice=int(input("enter your choice:"))
+
+
+    if choice==1:
+        study()
+    elif choice==2:
+        motivation()
+    elif choice==3:
+        about_her()
+    elif choice==4:
+        exit()
+        running=False
+    else:
+        print("invalid choice!!\n please select between 1 to 4")
+
+    if running:
+         print("=========================================\n=========================================")
+         print("Anything else")
+         print("=========================================\n=======================================")
+          
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
